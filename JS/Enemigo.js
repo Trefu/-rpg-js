d6ea5dc0a5db347b9f52857c7230ae9b9ca06daf
trelopez <tre.lopez@hotmail.com>
2020-09-26 19:55:57 -0300

    agregado stage de pelea,boton,display de stats de player y enemigo, generador de enemigos

--- JS/Enemigo.js
+++ JS/Enemigo.js
@@ -1,11 +1,10 @@
 let enemy;
 
-function Enemy(enemyType, hp, armor, strength, constitution, dexterity, intelligence, weapon) {
+function Enemy(classType, hp, armor, strength, dexterity, intelligence, weapon) {
     this.classType = classType,
         this.hp = hp,
         this.armor = armor,
         this.strength = strength,
-        this.constitution = constitution,
         this.dexterity = dexterity,
         this.intelligence = intelligence,
         this.weapon = weapon
--- JS/GameTest.js
+++ JS/GameTest.js
@@ -1,6 +1,44 @@
-let daga = 4;
-let baston = 4;
-let espadon = 12;
+let shortSword = {
+    "name": "shortSword",
+    "damage": 6,
+    "crit": 20
+};
+
+let bite = {
+    "name": "Bite",
+    "damage": 4,
+    "crit": 20
+};
+
+let spores = {
+    "name": "Spores",
+    "damage": 8,
+    "crit": 20
+};
+
+
+let claws = {
+    "name": "Claws",
+    "damage": 8,
+    "crit": 20
+};
+
+
+let staff = {
+    "name": "Staff",
+    "damage": 4,
+    "crit": 20
+};
+let dagger = {
+    "name": "Dagger",
+    "damage": 4,
+    "crit": [18, 19, 20]
+};
+let greatsword = {
+    "name": "Greatsword",
+    "damage": 12,
+    "crit": 20
+};
 let Manager = {
     setGameStart: function (classType) {
         this.resetPlayer(classType);
@@ -10,21 +48,80 @@ let Manager = {
         switch (classType) {
             //(classType, hp, armor, strength, dexterity, intelligence, weapon) 
             case "fighter":
-                player = new Player(classType, 12, 15, 3, -1, -1, espadon)
+                player = new Player(classType, 12, 17, 3, -1, -1, greatsword)
                 break;
             case "mage":
-                player = new Player(classType, 8, 10, 0, 3, 1, baston)
+                player = new Player(classType, 8, 11, -1, 1, 3, staff)
                 break;
             case "rogue":
-                player = new Player(classType, 10, 12, -1, 1, 3, daga)
+                player = new Player(classType, 10, 14, -1, 3, 1, dagger)
                 break;
         }
-
+        let getHeader = document.getElementById("header");
+        getHeader.innerHTML = "<h1>Fight!</h1>";
+        let getInterface = document.getElementById("interface");
+        getInterface.innerHTML = `<img class="avatarFight" src="imgs/avatars/${classType}.png">
+        <div class="class2">
+        <h3>${classType}</h3>
+        <p>Health: ${player.hp}</p>
+        <p>Armor: ${player.armor}</p>
+        <p>Strength: ${player.strength}</p>
+        <p>Dexterity: ${player.dexterity}</p>
+        <p>Intelligence: ${player.intelligence}</p>
+        <p>Weapon: ${player.weapon.name} Damage= 1d${player.weapon.damage}</p>
+        </div>
+        <div id="actions"></div>
+        <div id="arena"> <img src="imgs/arena.jpg" alt="arena" class="arena"> </div>
+        <div id="enemy" class="enemy"></div>
+        `;
 
     },
-
     setPrefight: function () {
+        getActions = document.getElementById("actions");
+        getArena = document.getElementById("arena");
+        getActions.innerHTML = `
+        <a href="#" class="btn" onclick="Manager.setFight()">Search an enemy!</a>
+        `;
+    },
+    //crear enemigo aleatorio
 
+    setFight: function () {
+        let enemy01 = new Enemy("Goblin", 7, 15, -1, 2, 0, shortSword);
+        let enemy02 = new Enemy("Violet Fungus", 18, 5, -4, -4, -5, spores);
+        let enemy03 = new Enemy("Wolf", 11, 13, +1, +2, -4, bite);
+        let enemy04 = new Enemy("Gnoll", 22, 15, +2, +1, -2, shortSword);
+        let enemy05 = new Enemy("Ghoul", 22, 12, +1, +2, -2, claws);
+        let pickEnemy = Math.floor(Math.random() * 5 + 1);
+        console.log(pickEnemy);
+        switch (pickEnemy) {
+            case 1:
+                enemy = enemy01
+                break;
+            case 2:
+                enemy = enemy02
+                break;
+            case 3:
+                enemy = enemy03
+                break;
+            case 4:
+                enemy = enemy04
+                break;
+            case 5:
+                enemy = enemy05
+                break;
+        }
+        let getEnemyInterface = document.getElementById("enemy");
+        getEnemyInterface.innerHTML = `<img class="avatarFight" src="imgs/enemy/${enemy.classType}.png">
+        <div class="class2">
+        <h3>${enemy.classType}</h3>
+        <p>Health: ${enemy.hp}</p>
+        <p>Armor: ${enemy.armor}</p>
+        <p>Strength: ${enemy.strength}</p>
+        <p>Dexterity: ${enemy.dexterity}</p>
+        <p>Intelligence: ${enemy.intelligence}</p>
+        <p>Weapon: ${enemy.weapon.name} Damage= 1d${enemy.weapon.damage}</p>
+        </div>
+        `;
     }
 };
 
