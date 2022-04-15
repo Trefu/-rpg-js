a399a89657b6a924f8f08e629b76c20fa9d5b57b
Trefu <tre.lopez@hotmail.com>
2022-04-15 12:41:11 -0300

    simplificacion de clases borro warrior

--- JS/models/players/BaseModel.js
+++ JS/models/players/BaseModel.js
@@ -1,11 +1,9 @@
 export default class BaseModel {
-    constructor(name, classCharacter) {
-        this.classCharacter = classCharacter;
+    constructor(name) {
         this.name = name;
         this.maxHealth = 0;
         this.health = 0;
         this.attack = 0;
-
     }
     attack() {
         console.log('attack')
