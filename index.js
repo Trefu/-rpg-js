8441f3ec7d99f227a82b58c49cb247f106093834
Trefu <tre.lopez@hotmail.com>
2022-04-15 19:02:23 -0300

    router

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import BaseModel from "./models/players/BaseModel.js";
+import BaseModel from "./JS/models/players/BaseModel.js";
 
-import GameManager from "./GameManager.js";
+import GameManager from "./JS/GameManager.js";
 
-const SELECT_BATTLE_MASTER_BUTTON = document.getElementById('battlemaster-select-btn');
+/* const SELECT_BATTLE_MASTER_BUTTON = document.getElementById('battlemaster-select-btn');
 const SELECT_SHADOW_MASTER_BUTTON = document.getElementById('shadowmaster-select-btn');
 const SELECT_SPELL_MASTER_BUTTON = document.getElementById('spellmaster-select-btn');
 
 SELECT_BATTLE_MASTER_BUTTON.addEventListener('click', GameManager.handlerClassSelect);
 SELECT_SHADOW_MASTER_BUTTON.addEventListener('click', GameManager.handlerClassSelect);
-SELECT_SPELL_MASTER_BUTTON.addEventListener('click', GameManager.handlerClassSelect);
+SELECT_SPELL_MASTER_BUTTON.addEventListener('click', GameManager.handlerClassSelect); */
