c66aa47ce8992b55c386815b1b74ed7de87d44c2
Trefu <tre.lopez@hotmail.com>
2022-04-15 15:38:26 -0300

    nuevos estilos para los botones, player movido a gamemanager, modifico estilos

--- JS/GameManager.js
+++ JS/GameManager.js
@@ -1,4 +1,10 @@
 //Primero declaro la variable y luego la exporto, esto se hacen con las constantes
+import Battlemaster from "./models/players/Battlemaster.js";
+import Shadowmaster from "./models/players/Shadowmaster.js";
+import Spellmaster from "./models/players/Spellmaster.js";
+
+var player = null;
+
 const GameManager = {
     handlerClassSelect: function (event) {
         let selectedClass = event.target.getAttribute("data-class");
--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,6 @@
 import BaseModel from "./models/players/BaseModel.js";
-import Battlemaster from "./models/players/Battlemaster.js";
-import Shadowmaster from "./models/players/Shadowmaster.js";
-import Spellmaster from "./models/players/Spellmaster.js";
-import GameManager from "./GameManager.js";
 
-var player = null;
+import GameManager from "./GameManager.js";
 
 const SELECT_BATTLE_MASTER_BUTTON = document.getElementById('battlemaster-select-btn');
 const SELECT_SHADOW_MASTER_BUTTON = document.getElementById('shadowmaster-select-btn');
@@ -13,6 +9,3 @@ const SELECT_SPELL_MASTER_BUTTON = document.getElementById('spellmaster-select-b
 SELECT_BATTLE_MASTER_BUTTON.addEventListener('click', GameManager.handlerClassSelect);
 SELECT_SHADOW_MASTER_BUTTON.addEventListener('click', GameManager.handlerClassSelect);
 SELECT_SPELL_MASTER_BUTTON.addEventListener('click', GameManager.handlerClassSelect);
-
-
-console.log(player)
